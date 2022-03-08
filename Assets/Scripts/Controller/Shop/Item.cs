41d46ec509b8ebe28b1c6af591534342f7af9eef
bao <baodq@taap.vn>
2022-03-08 10:57:11 +0700

    fix shop

---
+++ Assets/Scripts/Controller/Shop/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    int ID = 0;
+    int Name;
+    int Price = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SetID() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
